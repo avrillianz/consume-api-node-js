17f1cee00b68e343209e83677b2f3bbba910ed5d
Septian Akbar Widya Dwi Putra <avrillianz@yahoo.co.id>
2021-01-14 12:47:08 +0700

    Update service.js

--- service.js
+++ service.js
@@ -3,42 +3,42 @@ const https = require('https');
 
 exports.sampleRequest = function (req, res) {
     const reqUrl = url.parse(req.url, true);
-    var repository = 'avrillianz';
+    var owner = 'avrillianz';
+    var repository = 'Intention';
+
+    if (reqUrl.query.owner) {
+        owner = reqUrl.query.owner
+    }
+
     if (reqUrl.query.repo) {
         repository = reqUrl.query.repo
     }
 
-    var response = {
-        "text": "Hello " + repository
-    };
-
     var options = {
         host: "api.github.com",
-        path: "/repos/avrillianz/Intention",
+        path: "/repos/" + owner + "/" + repository,
         headers: {
             "Accept": "application/vnd.github.v3+json",
             'User-Agent': 'Localhost'
         }
     }
 
-    https.get(options, (resp) => {
+    var request = https.get(options, (resp) => {
         let data = '';
 
-        // A chunk of data has been received.
         resp.on('data', (chunk) => {
             data += chunk;
         });
 
-        // The whole response has been received. Print out the result.
         resp.on('end', () => {
             console.log(JSON.stringify(data));
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(data);
         });
 
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     });
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(response));
 };
